b4faba2e78576df3c40c6d7979f1f529b467fadb
waterswat <waterswat@outlook.com>
2022-01-09 16:48:27 -0500

    Ignore compiled public, Cache console

--- Logger.mjs
+++ Logger.mjs
@@ -5,27 +5,31 @@ export const LOG_WARN = 3;
 export const LOG_ERROR = 4;
 export const LOG_SILENT = 5;
 
-// CACHE THE CONSOLE
-// CONSOLE.CONTEXT ISOLATE
-// FIREFOX
+const trace = console.trace.bind(console);
+const debug = console.debug.bind(console);
+const info = console.info.bind(console);
+const warn = console.warn.bind(console);
+const error = console.error.bind(console);
+
+
 export class Logger {
 	levels = ['trace','debug','info','warn','error','silent'];
 	constructor(tomp){
 		this.tomp = tomp;
 	}
 	trace(...args){
-		if(this.tomp.loglevel <= 0)if(!this.tomp.silent)console.trace('[TOMP]', ...args);
+		if(this.tomp.loglevel <= 0)if(!this.tomp.silent)trace('[TOMP]', ...args);
 	}
 	debug(...args){
-		if(this.tomp.loglevel <= 1)console.debug('[TOMP]', ...args);
+		if(this.tomp.loglevel <= 1)debug('[TOMP]', ...args);
 	}
 	info(...args){
-		if(this.tomp.loglevel <= 2)console.info('[TOMP]', ...args);
+		if(this.tomp.loglevel <= 2)info('[TOMP]', ...args);
 	}
 	warn(...args){
-		if(this.tomp.loglevel <= 3)console.warn('[TOMP]', ...args);
+		if(this.tomp.loglevel <= 3)warn('[TOMP]', ...args);
 	}
 	error(...args){
-		if(this.tomp.loglevel <= 4)console.error('[TOMP]', ...args);
+		if(this.tomp.loglevel <= 4)error('[TOMP]', ...args);
 	}
 };
--- public/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log("This will be the client.")})();
