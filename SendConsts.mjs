df29accdcc8b61c013b2bd4cb849ef3a9c7498b6
waterswat <waterswat@outlook.com>
2022-01-18 21:30:16 -0500

    further isolate tomp headers

--- SendConsts.mjs
+++ SendConsts.mjs
@@ -1,2 +1,2 @@
-export const header_json_prefix = 'x-json-';
-export const header_real_prefix = 'x-real-';
+export const header_json_prefix = 'x-tompjson-';
+export const header_real_prefix = 'x-tompreal-';
