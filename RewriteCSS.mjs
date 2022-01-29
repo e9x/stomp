1348d3644a00c340e118c2ccdffa2d3b12b4a8a6
e9x <x@sys32.dev>
2022-01-29 00:51:25 -0500

    unwrap css

--- RewriteCSS.mjs
+++ RewriteCSS.mjs
@@ -35,8 +35,25 @@ export class RewriteCSS {
 
 		return generate(ast);
 	}
-	unwrap(code, url){
-		return code;
+	unwrap(code, url, inline){
+		try{
+			var ast = parse(code, { context: inline ? 'declarationList' : 'stylesheet' });
+		}catch(err){
+			if(err instanceof SyntaxError){
+				return `/*${JSON.stringify(err.message)}*/`;
+			}else throw err;
+		}
+
+		const that = this;
+
+		walk(ast, function(node, item, list){
+			if(node.type === 'Url'){
+				if(this.atrule?.name == 'import')node.value = that.tomp.css.unwrap_serving(node.value, url);
+				else node.value = that.tomp.binary.unwrap_serving(node.value, url);
+			}
+		});
+
+		return generate(ast);
 	}
 	serve(serve, url){
 		serve = serve.toString();
