97925389a5f388ccb711730d5ee9f1c3cf0798e6
e9x <x@sys32.dev>
2022-01-30 18:17:10 -0500

    window.open

--- Client/PageClient.mjs
+++ Client/PageClient.mjs
@@ -4,6 +4,8 @@ import { DOMRewrite } from './Rewrites/DOM.mjs';
 import { StorageRewrite } from './Rewrites/Storage.mjs';
 import { CookieRewrite } from './Rewrites/Cookie.mjs';
 import { PageRequestRewrite } from './Rewrites/PageRequest.mjs';
+import { Reflect, wrap_function } from './RewriteUtil.mjs';
+import { global } from '../Global.mjs';
 
 export class PageClient extends Client {
 	static type = 'page';
@@ -15,5 +17,11 @@ export class PageClient extends Client {
 		new DOMRewrite(this).work();
 		new CookieRewrite(this).work();
 		new PageRequestRewrite(this).work();
+
+		global.open = wrap_function(global.open, (target, that, [ url, tar, features ]) => {
+			url = new URL(url, this.location.proxy);
+			url = this.tomp.html.serve(url, this.location.proxy);
+			return Reflect.apply(target, that, [ url, tar, features ]);
+		});
 	}
 };
