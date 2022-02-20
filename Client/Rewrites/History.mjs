a88b792bec2399b3f459acc65d436e4f6dcd043a
David Reed <x@sys32.dev>
2022-02-19 23:09:47 -0500

    use barebones history proxy

--- Client/Rewrites/History.mjs
+++ Client/Rewrites/History.mjs
@@ -1,62 +1,25 @@
 import { Rewrite } from '../Rewrite.mjs';
 import { global } from '../../Global.mjs';
-import { getOwnPropertyDescriptors, Reflect } from '../RewriteUtil.mjs';
+import { Reflect, wrap_function } from '../RewriteUtil.mjs';
 
 export class HistoryRewrite extends Rewrite {
+	global = global.History;
+	handler(target, that, args){
+		if(args.length <= 2){
+			throw new TypeError(`Failed to execute '${target.name}' on 'History': 2 arguments required, but only ${args.length} present.`);
+		}
+
+		let [data, title, url] = args;
+		
+		if(url != undefined){
+			url = String(url);
+			url = this.client.tomp.html.serve(new URL(url, this.client.base), this.client.base);
+		}
+		
+		return Reflect.apply(target, that, [ data, title, url ]);
+	}
 	work(){
-		const that = this;
-		const _History = global.History;
-		const instances = [global.history];
-		const {length,scrollRestoration,state} = getOwnPropertyDescriptors(_History.prototype);
-
-		class History {
-			constructor(){
-				throw new TypeError('Illegal constructor');
-			}
-			[Symbol.toStringTag] = 'History';
-			back(){
-				return _History.prototype.back.call(this);
-			}
-			forward(){
-				return _History.prototype.forward.call(this);
-			}
-			go(delta){
-				return _History.prototype.go.call(this, delta);
-			}
-			pushState(data, title, url){
-				if(url != undefined){
-					url = url.toString();
-					url = that.client.tomp.html.serve(new URL(url, that.client.base), that.client.base);
-				}
-				
-				return Reflect.apply(_History.prototype.pushState, this, [ data, title, url ]);
-			}
-			replaceState(data, title, url){
-				if(url != undefined){
-					url = url.toString();
-					url = that.client.tomp.html.serve(new URL(url, that.client.base), that.client.base);
-				}
-				
-				return Reflect.apply(_History.prototype.replaceState, this, [ data, title, url ]);
-			}
-			get length(){
-				return length.get.call(this);
-			}
-			get scrollRestoration(){
-				return scrollRestoration.get.call(this);
-			}
-			get state(){
-				return state.get.call(this);
-			}
-		};
-
-		Reflect.defineProperty(global, 'History', {
-			configurable: true,
-			enumerable: false,
-			value: History,
-			writable: true,
-		});
-
-		Reflect.setPrototypeOf(global.history, History.prototype);
+		this.global.prototype.pushState = wrap_function(this.global.prototype.pushState, this.handler.bind(this));
+		this.global.prototype.replaceState = wrap_function(this.global.prototype.replaceState, this.handler.bind(this));
 	}
 };
