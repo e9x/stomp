23121d89b5a752771f97be8bcc1998cab6b458c4
e9x <x@sys32.dev>
2022-04-07 20:09:01 -0400

    math

--- RewriteJS.js
+++ RewriteJS.js
@@ -56,30 +56,20 @@ class Modifications {
 		let offset = 0;
 
 		main: for (let [oldnode, newnode] of this.changes) {
-			/*if (replaced.has(oldnode)) {
-				console.log('REPLACED', generate(newnode));
-				continue;
-			}
-			console.log(
-				oldnode_range[0],
-				this.range_size(oldnode_range),
-				generate(newnode)
-			);*/
-			// insight
-
-			// console.log(`iterating for ${generate(newnode)}`);
+			console.log(`iterating for ${generate(newnode)}`);
 
 			for (let range of replaced) {
-				/*console.log(
+				console.log(
 					`\toldnode.range: ${oldnode.range} size: ${this.range_size(
 						oldnode.range
 					)}, range: ${range} size: ${this.range_size(range)}`
-				);*/
+				);
 				if (
 					range[0] <= oldnode.range[0] &&
+					range[1] >= oldnode.range[1] &&
 					this.range_size(range) > this.range_size(oldnode.range)
 				) {
-					// console.log('continue', generate(newnode));
+					console.log('continue', generate(newnode));
 					continue main;
 				}
 			}
