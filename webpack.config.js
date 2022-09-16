15141ab59489d66ebbe5983eda17a0df2076d1b7
David Reed <reedswork@proton.me>
2022-09-15 20:07:02 -0400

    webpack cli-ify

--- webpack.config.js
+++ webpack.config.js
@@ -1,119 +1,42 @@
 import webpack from 'webpack';
-import Events from 'node:events';
-import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
-export default class Builder {
-	get_errors(error, stats) {
-		const errors = [];
-
-		if (error) {
-			errors.push(error);
-		}
-
-		if (typeof stats == 'object' && stats !== undefined && stats !== null) {
-			for (let error of stats.compilation.errors) {
-				if (error?.module) {
-					errors.push(`${error.module?.request}: ${error}`);
-				} else {
-					errors.push(error);
-				}
-			}
-		}
-
-		return errors;
-	}
-	webpacks = [];
-	constructor(output, development) {
-		const mode = development ? 'development' : 'production';
-
-		const cache = {
+/**
+ * @type {webpack.Configuration[]}
+ */
+const config = [
+	{
+		mode: isDevelopment ? 'development' : 'production',
+		cache: {
 			type: 'filesystem',
-		};
+		},
+		devtool: isDevelopment ? 'eval' : 'source-map',
+		entry: {
+			client: './src/Client/entry.js',
+			worker: './src/Worker/entry.js',
+		},
+		output: {
+			path: fileURLToPath(new URL('../dist/', import.meta.url)),
+			filename: '[name].js',
+		},
+	},
+	{
+		mode: isDevelopment ? 'development' : 'production',
+		cache: {
+			type: 'filesystem',
+		},
+		devtool: 'source-map',
+		entry: './src/Bootstrapper/Bootstrapper.js',
+		output: {
+			library: 'StompBoot',
+			libraryTarget: 'umd',
+			libraryExport: 'default',
+			path: fileURLToPath(new URL('../dist/', import.meta.url)),
+			filename: 'bootstrapper.js',
+		},
+	},
+];
 
-		this.webpacks.push(
-			webpack({
-				mode,
-				cache,
-				devtool: 'source-map',
-				entry: {
-					client: join(__dirname, 'src', 'Client', 'entry.js'),
-					worker: join(__dirname, 'src', 'Worker', 'entry.js'),
-				},
-				context: __dirname,
-				output: {
-					path: output,
-					filename: '[name].js',
-				},
-			})
-		);
-
-		this.webpacks.push(
-			webpack({
-				mode,
-				cache,
-				devtool: 'source-map',
-				entry: join(__dirname, 'src', 'Bootstrapper', 'Bootstrapper.js'),
-				context: __dirname,
-				output: {
-					library: 'StompBoot',
-					libraryTarget: 'umd',
-					libraryExport: 'default',
-					path: output,
-					filename: 'bootstrapper.js',
-				},
-			})
-		);
-	}
-	build() {
-		return Promise.all(
-			this.webpacks.map(
-				webpack =>
-					new Promise((resolve, reject) => {
-						webpack.run((error, stats) => {
-							const errors = this.get_errors(error, stats);
-
-							if (errors.length) {
-								reject(errors);
-							} else {
-								resolve();
-							}
-						});
-					})
-			)
-		);
-	}
-	watch() {
-		const emitter = new Events();
-
-		const watch = Promise.all(
-			this.webpacks.map(
-				webpack =>
-					new Promise(resolve =>
-						setTimeout(() => {
-							resolve(
-								webpack.watch({}, (error, stats) => {
-									const errors = this.get_errors(error, stats);
-
-									if (errors.length) {
-										emitter.emit('error', errors);
-									} else {
-										emitter.emit('bulit');
-									}
-								})
-							);
-						})
-					)
-			)
-		);
-
-		emitter.stop = async () => {
-			(await watch).close();
-		};
-
-		return emitter;
-	}
-}
+export default config;
