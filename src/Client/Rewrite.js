334c5296894f9e8add05dd2e1118ced520be082c
David Reed <reedswork@proton.me>
2023-06-09 14:20:43 -0400

    annotate rewrite.client

--- src/Client/Rewrite.js
+++ src/Client/Rewrite.js
@@ -1,6 +1,10 @@
 // Base class for a client rewrite
 
 export default class Rewrite {
+	/**
+	 *
+	 * @param {import('./Client').default} client
+	 */
 	constructor(client) {
 		this.client = client;
 		this.tomp = this.client.tomp;
